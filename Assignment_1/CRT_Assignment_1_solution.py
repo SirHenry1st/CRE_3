413f10a3f2e9641347bae579ebbbf9fcf93ae132
SirHenry1st <bn-heuschmid29@web.de>
2026-04-28 16:02:36 +0200

    Added cumulative concentrartion for first plotting

--- Assignment_1/CRT_Assignment_1_solution.py
+++ Assignment_1/CRT_Assignment_1_solution.py
@@ -50,6 +50,7 @@ ax.plot(df["Exp_1_t"], df["Exp_1_conc_C"], label = {"Concentration C Exp. 1"}, c
 ax.plot(df["Exp_1_t"], df["Exp_1_conc_D"], label = {"Concentration D Exp. 1"}, color=iciw_colors.FLAME, marker = "o")
 ax.plot(df["Exp_1_t"], df["Exp_1_conc_E"], label = {"Concentration E Exp. 1"}, color=iciw_colors.DRAB, marker = "o")
 ax.plot(df["Exp_1_t"], df["Exp_1_conc_F"], label = {"Concentration F Exp. 1"}, color="magenta", marker = "o")
+ax.plot(df["Exp_1_t"], df["Exp_1_conc_A"]+df["Exp_1_conc_B"]+df["Exp_1_conc_C"]+df["Exp_1_conc_D"]+df["Exp_1_conc_E"]+df["Exp_1_conc_F"], label = {"Cumulative Concentration Exp. 1"}, color="cyan", marker = "o")
 ax.set_xlabel("t / s")
 ax.set_ylabel(r'$\mathrm{c}\; / \ \mathrm{\frac{mol}{m^3}}$')
 ax.legend()
@@ -63,6 +64,7 @@ ax.plot(df["Exp_2_t"], df["Exp_2_conc_C"], label = {"Concentration C Exp. 2"}, c
 ax.plot(df["Exp_2_t"], df["Exp_2_conc_D"], label = {"Concentration D Exp. 2"}, color=iciw_colors.FLAME, marker = "o")
 ax.plot(df["Exp_2_t"], df["Exp_2_conc_E"], label = {"Concentration E Exp. 2"}, color=iciw_colors.DRAB, marker = "o")
 ax.plot(df["Exp_2_t"], df["Exp_2_conc_F"], label = {"Concentration F Exp. 2"}, color="magenta", marker = "o")
+ax.plot(df["Exp_2_t"], df["Exp_2_conc_A"]+df["Exp_2_conc_B"]+df["Exp_2_conc_C"]+df["Exp_2_conc_D"]+df["Exp_2_conc_E"]+df["Exp_2_conc_F"], label = {"Cumulative Concentration Exp. 2"}, color="cyan", marker = "o")
 ax.set_xlabel("t / s")
 ax.set_ylabel(r'$\mathrm{c}\; / \ \mathrm{\frac{mol}{m^3}}$')
 ax.legend()
@@ -76,6 +78,7 @@ ax.plot(df["Exp_3_t"], df["Exp_3_conc_C"], label = {"Concentration C Exp. 3"}, c
 ax.plot(df["Exp_3_t"], df["Exp_3_conc_D"], label = {"Concentration D Exp. 3"}, color=iciw_colors.FLAME, marker = "o")
 ax.plot(df["Exp_3_t"], df["Exp_3_conc_E"], label = {"Concentration E Exp. 3"}, color=iciw_colors.DRAB, marker = "o")
 ax.plot(df["Exp_3_t"], df["Exp_3_conc_F"], label = {"Concentration F Exp. 3"}, color="magenta", marker = "o")
+ax.plot(df["Exp_3_t"], df["Exp_3_conc_A"]+df["Exp_3_conc_B"]+df["Exp_3_conc_C"]+df["Exp_3_conc_D"]+df["Exp_3_conc_E"]+df["Exp_3_conc_F"], label = {"Cumulative Concentration Exp. 3"}, color="cyan", marker = "o")
 ax.set_xlabel("t / s")
 ax.set_ylabel(r'$\mathrm{c}\; / \ \mathrm{\frac{mol}{m^3}}$')
 ax.legend()
@@ -89,6 +92,7 @@ ax.plot(df["Exp_4_t"], df["Exp_4_conc_C"], label = {"Concentration C Exp. 4"}, c
 ax.plot(df["Exp_4_t"], df["Exp_4_conc_D"], label = {"Concentration D Exp. 4"}, color=iciw_colors.FLAME, marker = "o")
 ax.plot(df["Exp_4_t"], df["Exp_4_conc_E"], label = {"Concentration E Exp. 4"}, color=iciw_colors.DRAB, marker = "o")
 ax.plot(df["Exp_4_t"], df["Exp_4_conc_F"], label = {"Concentration F Exp. 4"}, color="magenta", marker = "o")
+ax.plot(df["Exp_4_t"], df["Exp_4_conc_A"]+df["Exp_4_conc_B"]+df["Exp_4_conc_C"]+df["Exp_4_conc_D"]+df["Exp_4_conc_E"]+df["Exp_4_conc_F"], label = {"Cumulative Concentration Exp. 4"}, color="cyan", marker = "o")
 ax.set_xlabel("t / s")
 ax.set_ylabel(r'$\mathrm{c}\; / \ \mathrm{\frac{mol}{m^3}}$')
 ax.legend()
@@ -102,6 +106,7 @@ ax.plot(df["Exp_5_t"], df["Exp_5_conc_C"], label = {"Concentration C Exp. 5"}, c
 ax.plot(df["Exp_5_t"], df["Exp_5_conc_D"], label = {"Concentration D Exp. 5"}, color=iciw_colors.FLAME, marker = "o")
 ax.plot(df["Exp_5_t"], df["Exp_5_conc_E"], label = {"Concentration E Exp. 5"}, color=iciw_colors.DRAB, marker = "o")
 ax.plot(df["Exp_5_t"], df["Exp_5_conc_F"], label = {"Concentration F Exp. 5"}, color="magenta", marker = "o")
+ax.plot(df["Exp_5_t"], df["Exp_5_conc_A"]+df["Exp_5_conc_B"]+df["Exp_5_conc_C"]+df["Exp_5_conc_D"]+df["Exp_5_conc_E"]+df["Exp_5_conc_F"], label = {"Cumulative Concentration Exp. 5"}, color="cyan", marker = "o")
 ax.set_xlabel("t / s")
 ax.set_ylabel(r'$\mathrm{c}\; / \ \mathrm{\frac{mol}{m^3}}$')
 ax.legend()
@@ -115,6 +120,7 @@ ax.plot(df["Exp_6_t"], df["Exp_6_conc_C"], label = {"Concentration C Exp. 6"}, c
 ax.plot(df["Exp_6_t"], df["Exp_6_conc_D"], label = {"Concentration D Exp. 6"}, color=iciw_colors.FLAME, marker = "o")
 ax.plot(df["Exp_6_t"], df["Exp_6_conc_E"], label = {"Concentration E Exp. 6"}, color=iciw_colors.DRAB, marker = "o")
 ax.plot(df["Exp_6_t"], df["Exp_6_conc_F"], label = {"Concentration F Exp. 6"}, color="magenta", marker = "o")
+ax.plot(df["Exp_6_t"], df["Exp_6_conc_A"]+df["Exp_6_conc_B"]+df["Exp_6_conc_C"]+df["Exp_6_conc_D"]+df["Exp_6_conc_E"]+df["Exp_6_conc_F"], label = {"Cumulative Concentration Exp. 6"}, color="cyan", marker = "o")
 ax.set_xlabel("t / s")
 ax.set_ylabel(r'$\mathrm{c}\; / \ \mathrm{\frac{mol}{m^3}}$')
 ax.legend()
@@ -123,4 +129,9 @@ plt.show()
 # %%
 # Reaction hypothesis
 
+# A + B -> C
+# C -> D
+# A + C -> E
+# D -> F
+# all stoichiometry is 1 might be correct, but not sure about that
 
