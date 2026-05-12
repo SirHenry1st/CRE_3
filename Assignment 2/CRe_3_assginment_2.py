710c0499f0fff6267555df41cf01a657ceceaba8
Benjamin Heuschmid <bn-heuschmid29@web.de>
2026-05-12 14:52:21 +0200

    First file with import

---
+++ Assignment 2/CRe_3_assginment_2.py
@@ -0,0 +1,10 @@
+#%%
+# Imports of packages
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import ICIW_Plots.colors as iciw_colors
+import CoolProp as CP
+
+#%%
