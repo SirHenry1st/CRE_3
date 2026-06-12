90d49127f36527b70ffc8a180df1e3cdab0a6a94
Noah1342020 <noahherrscher@gmail.com>
2026-06-12 21:27:13 +0100

    thermodynamics 2

--- Assignment 3/CRE_3_assignment_3_Noah.py
+++ Assignment 3/CRE_3_assignment_3_Noah.py
@@ -10,36 +10,43 @@ M_WA = 18.01 # Molar mass of water in g/mol
 M_EG = 62.07 # Molar mass of ethylene glycol in g/mol
 M_EO = 44.05 # Molar mass of ethylene oxide in g/mol
 
-R_WA = 8.314 / M_WA # Specific heat capacity of water in J/mol-K
-R_EG = 8.314 / M_EG # Specific heat capacity of ethylene glycol in J/mol-K
-R_EO = 8.314 / M_EO # Specific heat capacity of ethylene oxide in J/mol-K
+R_WA = 8.314 / M_WA # Specific heat capacity of water in 
+R_EG = 8.314 / M_EG # Specific heat capacity of ethylene glycol in 
+R_EO = 8.314 / M_EO # Specific heat capacity of ethylene oxide in 
 
-T_C_EG = 724.05 # Critical temperature of ethylene glycol
+T_C_EG = 724.05 # Critical temperature of ethylene glycol in K
 T_C_WA = 647.10 # Critical temperature of water in K 
 T_C_EO = 469.15 # Critical temperature of ethylene oxide in K 
 
 c_p_EG_params = np.array([0, 33.1585, -25.9580, 0, 0, 0]) # Parameters for the specific heat capacity of ethylene glycol as a function of temperature
 c_p_WA_params = np.array([0.2399, 12.8647, -33.6392, 104.7686, -155.4709, 92.3726]) # Parameters for the specific heat capacity of water as a function of temperature
-c_p_EO_params = np.array([0.5144, 9.1319, 5.0866, -14.4010, -5.0022, 23.4152]) # Parameters for the specific heat capacity of ethylene oxide as a function of temperature (not used in this code)
 
-# Heat capacity of ethylene glycol dependent on temperature
+# Heat capacity of ethylene glycol dependent on temperature in J/kg-K
 def c_p_EG(T, c_p_EG_params):
     return R_EG * (c_p_EG_params[0] / (1- (T / T_C_EG)) + c_p_EG_params[1] + c_p_EG_params[2] * (1 - (T / T_C_EG)) + c_p_EG_params[3] * (1 - (T / T_C_EG))**2 + c_p_EG_params[4] * (1 - (T / T_C_EG))**3 + c_p_EG_params[5] * (1 - (T / T_C_EG))**4) * 1000
 
-# Heat capacity of water dependent on temperature
+# Heat capacity of water dependent on temperature in J/kg-K
 def c_p_WA(T, c_p_WA_params):
     return R_WA * (c_p_WA_params[0] / (1- (T / T_C_WA)) + c_p_WA_params[1] + c_p_WA_params[2] * (1 - (T / T_C_WA)) + c_p_WA_params[3] * (1 - (T / T_C_WA))**2 + c_p_WA_params[4] * (1 - (T / T_C_WA))**3 + c_p_WA_params[5] * (1 - (T / T_C_WA))**4) * 1000
 
-# Heat capacity of ethylene oxide dependent on temperature
-def c_p_EO(T, c_p_EO_params):
-    return R_EO * (c_p_EO_params[0] / (1- (T / T_C_EO)) + c_p_EO_params[1] + c_p_EO_params[2] * (1 - (T / T_C_EO)) + c_p_EO_params[3] * (1 - (T / T_C_EO))**2 + c_p_EO_params[4] * (1 - (T / T_C_EO))**3 + c_p_EO_params[5] * (1 - (T / T_C_EO))**4) * 1000
+c_p_EO_values = np.array([1.88, 1.91, 1.95, 2.00, 2.06, 2.15, 2.27, 2.40]) * 1000 # Specific heat capacity values for ethylene oxide in J/kg-K at the corresponding temperature values in
+T_EO_values = np.array([-40, -20, 0, 20, 40, 60, 80, 100]) + 273.15 # Temperature values for the specific heat capacity of ethylene oxide in K
+
+# Interpolate the specific heat capacity of ethylene oxide as a function of temperature
+c_p_EO_interp = interp1d(T_EO_values, c_p_EO_values, kind='linear', fill_value=(c_p_EO_values[0], c_p_EO_values[-1]), bounds_error=False) 
 
 
 # Ideal mixture of specific heat capacities in J kg-1 K-1
 def c_p_mix(T, c_p_EG_params, c_p_WA_params, x_arr):
     x_EG = x_arr[0]
     x_WA = x_arr[1]
-    return x_EG * c_p_EG(T, c_p_EG_params) + x_WA * c_p_WA(T, c_p_WA_params)
+    x_EO = x_arr[2]
+
+    w_EG = w(x_arr, x_EG, M_EG)
+    w_WA = w(x_arr, x_WA, M_WA)
+    w_EO = w(x_arr, x_EO, M_EO)
+
+    return w_EG * c_p_EG(T, c_p_EG_params) + w_WA * c_p_WA(T, c_p_WA_params) + w_EO * c_p_EO_interp(T)
 
 rho_EG_params = np.array([1305.5931, -1374.2561, 1691.0501, -665.0358]) # Parameters for the density of ethylene glycol as a function of temperature
 rho_WA_params = np.array([1094.0233, -1813.2295, 3863.9557, -2479.8130]) # Parameters for the density of water as a function of temperature
@@ -87,35 +94,35 @@ def x_i(c_arr, c_i):
     return c_i / np.sum(c_arr)
 
 fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(30, 12))
-T_range = np.arange(293, 450, 1) 
-ax[0, 0].plot(T_range, c_p_EG(T_range, c_p_EG_params), label='Ethylene Glycol')
-ax[0, 0].plot(T_range, c_p_WA(T_range, c_p_WA_params), label='Water')
-ax[0, 0].plot(T_range, c_p_EO(T_range, c_p_EO_params), label='Ethylene Oxide')
-ax[0, 0].set_xlabel('Temperature (K)')
+T_range = np.arange(20, 150, 1) + 273.15
+ax[0, 0].plot(T_range - 273.15, c_p_EG(T_range, c_p_EG_params), label='Ethylene Glycol')
+ax[0, 0].plot(T_range - 273.15, c_p_WA(T_range, c_p_WA_params), label='Water')
+ax[0, 0].plot(T_range - 273.15, c_p_EO_interp(T_range), label='Ethylene Oxide')
+ax[0, 0].set_xlabel('Temperature (°C)')
 ax[0, 0].set_ylabel('Specific Heat Capacity (J/kg-K)')
 ax[0, 0].set_title('Specific Heat Capacity vs. Temperature')
 ax[0, 0].legend()
 
-ax[0, 1].plot(T_range, rho_EG(T_range, rho_EG_params), label='Ethylene Glycol')
-ax[0, 1].plot(T_range, rho_WA(T_range, rho_WA_params), label='Water')
-ax[0, 1].plot(T_range, rho_EO(T_range, rho_EO_params), label='Ethylene Oxide')
-ax[0, 1].set_xlabel('Temperature (K)')
+ax[0, 1].plot(T_range - 273.15, rho_EG(T_range, rho_EG_params), label='Ethylene Glycol')
+ax[0, 1].plot(T_range - 273.15, rho_WA(T_range, rho_WA_params), label='Water')
+ax[0, 1].plot(T_range - 273.15, rho_EO(T_range, rho_EO_params), label='Ethylene Oxide')
+ax[0, 1].set_xlabel('Temperature (°C)')
 ax[0, 1].set_ylabel('Density (kg/m^3)')
 ax[0, 1].set_title('Density vs. Temperature')
 ax[0, 1].legend()
 
-ax[1, 0].plot(T_range, c_p_mix(T_range, c_p_EG_params, c_p_WA_params, np.array([0.5, 0.5, 0])), label='50% EG, 50% WA')
-ax[1, 0].plot(T_range, c_p_mix(T_range, c_p_EG_params, c_p_WA_params, np.array([0.5, 0, 0.5])), label='25% EG, 75% WA')
-ax[1, 0].plot(T_range, c_p_mix(T_range, c_p_EG_params, c_p_WA_params, np.array([0, 0.5, 0.5])), label='75% EG, 25% WA')
-ax[1, 0].set_xlabel('Temperature (K)')
+ax[1, 0].plot(T_range - 273.15, c_p_mix(T_range, c_p_EG_params, c_p_WA_params, np.array([0.5, 0.5, 0])), label='50 mol% EG, 50 mol% WA')
+ax[1, 0].plot(T_range - 273.15, c_p_mix(T_range, c_p_EG_params, c_p_WA_params, np.array([0.5, 0, 0.5])), label='50 mol% EG, 50 mol% EO')
+ax[1, 0].plot(T_range - 273.15, c_p_mix(T_range, c_p_EG_params, c_p_WA_params, np.array([0, 0.5, 0.5])), label='50 mol% EO, 50 mol% WA')
+ax[1, 0].set_xlabel('Temperature (°C)')
 ax[1, 0].set_ylabel('Specific Heat Capacity (J/kg-K)')
 ax[1, 0].set_title('Specific Heat Capacity of Mixture vs. Temperature')
 ax[1, 0].legend() 
 
-ax[1, 1].plot(T_range, rho_mix(T_range, rho_EG_params, rho_WA_params, rho_EO_params, np.array([0.5, 0.5, 0])), label='50% EG, 50% WA')
-ax[1, 1].plot(T_range, rho_mix(T_range, rho_EG_params, rho_WA_params, rho_EO_params, np.array([0.5, 0, 0.5])), label='25% EG, 75% WA')
-ax[1, 1].plot(T_range, rho_mix(T_range, rho_EG_params, rho_WA_params, rho_EO_params, np.array([0, 0.5, 0.5])), label='75% EG, 25% WA')
-ax[1, 1].set_xlabel('Temperature (K)')
+ax[1, 1].plot(T_range - 273.15, rho_mix(T_range, rho_EG_params, rho_WA_params, rho_EO_params, np.array([0.5, 0.5, 0])), label='50 mol% EG, 50 mol% WA')
+ax[1, 1].plot(T_range - 273.15, rho_mix(T_range, rho_EG_params, rho_WA_params, rho_EO_params, np.array([0.5, 0, 0.5])), label='50 mol% EG, 50 mol% EO')
+ax[1, 1].plot(T_range - 273.15, rho_mix(T_range, rho_EG_params, rho_WA_params, rho_EO_params, np.array([0, 0.5, 0.5])), label='50 mol% EO, 50 mol% WA')
+ax[1, 1].set_xlabel('Temperature (°C)')
 ax[1, 1].set_ylabel('Density (kg/m^3)')
 ax[1, 1].set_title('Density of Mixture vs. Temperature')
 ax[1, 1].legend()
